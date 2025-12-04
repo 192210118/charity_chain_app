1a039479b4f5a6f8bdf075b63997000a0168316b
192210118 <124856217+192210118@users.noreply.github.com>
2025-12-04 08:11:48 +0530

    Create SignInActivity.kt

---
+++ SignInActivity.kt
@@ -0,0 +1,47 @@
+package com.example.charitychainapp.R
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.charitychainapp.ui.theme.CharityChainTheme
+
+class SignInActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            CharityChainTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    SignInScreen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SignInScreen() {
+    Text(
+        text = "Sign In Screen - Coming Soon!",
+        modifier = Modifier.fillMaxSize()
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SignInScreenPreview() {
+    CharityChainTheme {
+        SignInScreen()
+    }
+}
